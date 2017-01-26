503af851975c3eaf8ed98fa84c6d7fb705c9ad14
Mathias Mueller <mathias.mueller@drunomics.com>
2017-01-26 11:55:54 +0100

    Initial commit.

---
+++ src/Controller/EmbedController.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace Drupal\liveblog_embed\Controller;
+
+use Drupal\liveblog\LiveblogRendererInterface;
+
+use Drupal\Core\Controller\ControllerBase;
+
+use Drupal\yaem\Service\EmbedServiceInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * Class EmbedController.
+ */
+class EmbedController extends ControllerBase {
+
+  /**
+   * Embed service.
+   *
+   * @var EmbedServiceInterface
+   */
+  protected $embedService;
+
+  /**
+   * Ajax renderer service.
+   *
+   * @var LiveblogRendererInterface
+   */
+  protected $liveblogRenderer;
+
+  /**
+   * EmbedController constructor.
+   *
+   * @param EmbedServiceInterface $embedService
+   *   The service for fetching & rendering embeds.
+   * @param LiveblogRendererInterface $liveblogRenderer
+   *   The renderer for ajax requests.
+   */
+  public function __construct(EmbedServiceInterface $embedService, LiveblogRendererInterface $liveblogRenderer) {
+    $this->embedService = $embedService;
+    $this->liveblogRenderer = $liveblogRenderer;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get(YAEM_EMBED_SERVICE),
+      $container->get('liveblog.renderer')
+    );
+  }
+
+  /**
+   * Gets an embed for the given url.
+   *
+   * @param Request $request
+   *    The request.
+   *
+   * @return Response
+   *    The http/json response object.
+   *    - Response::HTTP_NO_CONTENT if url is not supported.
+   *    - JsonResponse with content otherwise.
+   */
+  public function embed(Request $request) {
+    $url = $request->query->get('url');
+    $content = [];
+
+    if ($element = $this->embedService->renderUrl($url)) {
+      $content = $this->liveblogRenderer->render($element);
+    }
+
+    if (empty($content)) {
+      return new Response('Invalid url: ' . $url, Response::HTTP_NO_CONTENT);
+    }
+
+    return new JsonResponse($content);
+  }
+
+}
